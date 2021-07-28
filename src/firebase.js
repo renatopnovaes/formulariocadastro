3fd7f0b0e67793f8f0b97730411f6a7d64228575
Renato Novaes <69921993+renatopnovaes@users.noreply.github.com>
2021-07-28 12:06:49 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,13 @@ import firebase from "firebase";
 
 
 var firebaseConfig = {
-   // Insert here your api key
+   apiKey: "AIzaSyB1qn3DFHOvCoTmegdn8WhRFPmfHLrjFaM",
+    authDomain: "mirai-ad14b.firebaseapp.com",
+    databaseURL: "https://mirai-ad14b-default-rtdb.firebaseio.com",
+    projectId: "mirai-ad14b",
+    storageBucket: "mirai-ad14b.appspot.com",
+    messagingSenderId: "592544590538",
+    appId: "1:592544590538:web:ba3015ef75042ad975e233"
   };
   // Initialize Firebase
   var fireDb =  firebase.initializeApp(firebaseConfig);
